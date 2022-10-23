bb58686205111278386607e5dd2adecdc16d94f5
Kyushi-CB <kyushi@crafterbase.de>
2022-10-23 14:06:29 +0200

    working on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,79 +1,166 @@
 #!/bin/bash
 
+# color templates
+WHITE="\e[0m"
+RED="\e[31m"
+GREEN="\e[32m"
+LIGHTYELLOW="\e[93m"
+LIGHTBLUE="\e[94m"
+BOLDRED="\e[1;31m"
+BOLDGREEN="\e[1;32m"
+
 # check if executing user is root or has sudo rights 
-if [ "$EUID" -ne 0 ]
-  then echo "Bitte führe dieses Script als root oder Sudo Benutzer aus!";
-  break;;
-  echo "Installer wird beendet";
-  exit;;
+if [ "$EUID" != 0 ]
+  then echo -e "[${BOLDRED}Error${WHITE}] Installation nur als root oder sudo Benutzer möglich!"
+  exit
 fi
 
 # check if OS is debian bullseye 64bit
 
 
-echo "Formatierungs-Champ Install Script v.1.0.0 for Raspbian64 / Debian Bullseye by https://github.com/Kyushi-CB/"
+echo -e "${LIGHTYELLOW}Formatierungs-Champ Install Script v.1.0.0 for Raspbian64/Debian Bullseye by Jack Höhndorf${WHITE}"
 echo "Dieses Install-Script macht aus deinem Gerät eine Formatierungsstation."
 
 # query if installer should proceed
-read -p "Mit der Installation beginnen? (Ja/Nein)" yn
-case $yn in 
-	[yYjJYesyesYESJajaJA] ) echo "Installation wird gestartet...";
-		break;;
-	[nNNonoNONeinneinNEIN] ) echo "Installer wird beendet...";
-		exit;;
-	* ) echo "Bitte antworte mit Ja oder Nein";;
-esac
+while true
+do
+	read -p "$(echo -e ${LIGHTBLUE}"Mit der Installation beginnen?"${WHITE}) (Ja/Nein):" yn
+	case $yn in 
+		[yY][eE][sS]|[jJ][aA]|[yY]|[jJ]) 
+			echo "Installation wird gestartet..."
+			break
+			;;	
+		[nN][oO]|[nN][eE][iI][nN]|[nN]) 
+			echo "Installer wird beendet..."
+			exit
+			;;
+		*) 
+			echo "Bitte antworte mit Ja/Nein | Yes/No"
+			;;
+	esac
 done
-
 # standart apt update/upgrade
 echo "Führe Update durch..."
-apt update && apt upgrade -y > /dev/null
-echo "done"
+{
+apt-get update && apt-get upgrade -y
+} > /dev/null
 
 # should the system be used as kiosk?
-read -p "Soll das Gerät als Kiosk eingerichtet werden? (Ja/Nein)" yn
-case $yn in 
-	[yYjJYesyesYESJajaJA] ) echo "Installation wird für Kiosk konfiguriert...";
-		# check if GUI is LXDE
-		# check if user kiosk exist
-			# if not: create user kiosk
-		# add user kiosk to autostart -> /etc/lightdm/lightdm.conf -> autologin-user=kiosk
+while true
+do
+	read -p "$(echo -e ${LIGHTBLUE}"Soll das Gerät als Kiosk eingerichtet werden?"${WHITE}) (Ja/Nein):" yn
+	case $yn in 
+		[yY][eE][sS]|[jJ][aA]|[yY]|[jJ])
+			echo "Installation wird für Kiosk konfiguriert..."
+			# check if GUI is LXDE
+			# check if user kiosk exist
+				# if not: create user kiosk
+			# add user kiosk to autostart -> /etc/lightdm/lightdm.conf -> autologin-user=kiosk
 
-		# check if unclutter is installed
-			# if not: install unclutter
-		# add unclutter to autostart ->  /etc/xdg/lxsession/LXDE/autostart -> @unclutter -idle 1
+			# check if unclutter is installed
+				# if not: install unclutter
+			# add unclutter to autostart ->  /etc/xdg/lxsession/LXDE/autostart -> @unclutter -idle 1
 
-		# check if chromium is installed
-			# if not: install chromium
-		# add chromium to autostart -> /etc/xdg/lxsession/LXDE/autostart -> chromium-browser --kiosk http:/127.0.0.1
-		# configure chrome startpage to localhost
-	[nNNonoNONeinneinNEIN] ) echo "Überspringe Kioskkonfiguration...";
-		break;;
-	* ) echo "Bitte antworte mit Ja oder Nein";;
-esac
+			# check if chromium is installed
+				# if not: install chromium
+			# add chromium to autostart -> /etc/xdg/lxsession/LXDE/autostart -> chromium-browser --kiosk http:/127.0.0.1
+			# configure chrome startpage to localhost
+			break
+			;;
+		[nN][oO]|[nN][eE][iI][nN]|[nN]) 
+			echo "Überspringe..."
+			break
+			;;
+		*) 
+			echo "Bitte antworte mit Ja/Nein | Yes/No"
+			;;
+	esac
 done
 
+echo "Es werden nun einige benötigte Programme installiert..."
 
-# check if git is installed
-	# if not: install git
+# check if sudo is installed / install sudo
+if [[ $(which sudo) == '' ]];
+then
+	echo "sudo wird installiert..."
+	{
+	apt-get install git -y
+	} > /dev/null
+else
+	echo "sudo ist bereits installiert, überspringe..."
+fi
 
-# check if hdparm is installed
-	# if not: install hdparm
+# check if Git is installed / install Git	
+if [[ $(which git) == '' ]];
+then
+	echo "Git wird installiert..."
+	{
+	apt-get install git -y
+	} > /dev/null
+else
+	echo "Git ist bereits installiert, überspringe..."
+fi
 
-# check if parted is installed
-	# if not: install parted
+# check if hdparm is installed / install hdparm
+if [[ $(which hdparm) == '' ]];
+then
+	echo "hdparm wird installiert..."
+	{
+	apt-get install hdparm -y
+	} > /dev/null	
+else
+	echo "hdparm ist bereits installiert, überspringe..."
+fi
 
-# check if apache2 is installed
-	# if not: install apache2
-# add to sudoers: www-data ALL=(ALL) NOPASSWD: /usr/sbin/hdparm, /usr/sbin/parted, usr/sbin/reboot, /usr/bin/apt
-# mkdir /var/www/formatierungs-champ
-# create apache config /etc/apache2/sites-enabled/fs.conf
-	# <VirtualHost *:80>
-	# 	DocumentRoot /var/www/formatierungs-champ
-	# 	ErrorLog ${APACHE_LOG_DIR}/error.log
-	# 	CustomLog ${APACHE_LOG_DIR}/access.log combined
-	# </VirtualHost>
+# check if parted is installed / install parted
+if [[ $(which parted) == '' ]];
+then
+	echo "Parted wird installiert..."
+	{
+	apt-get install parted -y
+	} > /dev/null	
+else
+	echo "Parted ist bereits installiert, überspringe..."
+fi
 
-# git -C '/var/www/formatierungs-champ' init
-# git -C '/var/www/formatierungs-champ' pull https://github.com/Kyushi-CB/formatierungs-champ
+# check if apache2 is installed / install apache2
+if [[ $(which apache2) == '' ]];
+then
+	echo "Apache2 wird installiert..."
+	{
+	apt-get install apache2 -y
+	} > /dev/null	
+else
+	echo "Apache2 ist bereits installiert, überspringe..."
+fi
+
+echo "setze Berechtigungen für den Benutzer: www-data..."
+
+# add www-data to sudoers
+echo "www-data ALL=(ALL) NOPASSWD: /usr/sbin/hdparm, /usr/sbin/parted, usr/sbin/reboot, /usr/bin/apt" | sudo EDITOR="tee -a" visudo
+
+# add directory "formatierungs-champ" to /var/www/
+echo "erstelle Verzeichniss /var/www/formatierungs-champ"
+{
+rm -R /var/www/formatierungs-champ
+mkdir /var/www/formatierungs-champ
+} > /dev/null 2>&1
+
+# create apache config
+echo "Konfiguriere Apache2..."
+touch /etc/apache2/sites-enabled/fs.conf
+echo "
+<VirtualHost *:80>
+ DocumentRoot /var/www/formatierungs-champ
+ ErrorLog ${APACHE_LOG_DIR}/error.log
+ CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>" >| /etc/apache2/sites-enabled/fs.conf
+
+# init git config
+echo "Git init..."
+git -C '/var/www/formatierungs-champ' init
+
+# pull repository from github
+echo "Holen des Repositorys von https://github.com/Kyushi-CB/formatierungs-champ"
+git -C '/var/www/formatierungs-champ' pull https://github.com/Kyushi-CB/formatierungs-champ
 
